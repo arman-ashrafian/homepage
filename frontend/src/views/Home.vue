a53bc6848640a13b8156cd1009c2f13290d31e30
arman-ashrafian <ashy63@gmail.com>
2018-08-03 16:59:58 -0700

    initial

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Mah Doods!"/>
+    <router-link to='/about'>About</router-link>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
