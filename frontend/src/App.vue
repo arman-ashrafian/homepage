5447e369eef0a6722af61ae0029e5486ea8d543d
arman-ashrafian <ashy63@gmail.com>
2018-08-12 20:23:10 -0700

    google drive auth

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app" class="container">
     <router-view/>
+    <a href="/GoogleLogin">Log in with Google</a>
   </div>
 </template>
 
--- server.go
+++ server.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/labstack/echo"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	drive "google.golang.org/api/drive/v3"
 )
 
 const (
 	port      = ":5500"
 	indexHTML = "frontend/dist/index.html"
+)
+
+var (
+	googleOauthConfig = &oauth2.Config{
+		RedirectURL:  "http://localhost:5500/GoogleCallback",
+		ClientID:     "41260821331-qv9lk54fshae7oal7973q6kj8mqmh3jl.apps.googleusercontent.com",
+		ClientSecret: "txCzHz5fUPFMiRcQ-nC2ihl-",
+		Scopes: []string{
+			"https://www.googleapis.com/auth/drive",
+			"https://www.googleapis.com/auth/drive.file",
+		},
+		Endpoint: google.Endpoint,
+	}
+	// Some random string, random for each request
+	oauthStateString = "random"
 )
 
 func main() {
@@ -22,5 +41,29 @@ func main() {
 	e.File("/", indexHTML)
 	e.File("/about", indexHTML)
 
+	// routes
+	e.GET("/GoogleLogin", handleGoogleLogin)
+	e.GET("/GoogleCallback", handleGoogleCallback)
+
 	e.Logger.Fatal(e.Start(port))
+}
+
+func handleGoogleLogin(c echo.Context) error {
+	url := googleOauthConfig.AuthCodeURL(oauthStateString)
+	c.Redirect(http.StatusTemporaryRedirect, url)
+	return nil
+}
+
+func handleGoogleCallback(c echo.Context) error {
+	code := c.FormValue("code")
+	tok, _ := googleOauthConfig.Exchange(oauth2.NoContext, code)
+	client := oauth2.NewClient(oauth2.NoContext, oauth2.StaticTokenSource(tok))
+
+	driveService, _ := drive.New(client)
+	fileList, _ := driveService.Files.List().Do()
+
+	for _, i := range fileList.Files {
+		fmt.Println(i.Name)
+	}
+	return c.String(http.StatusOK, "Fuck Yas")
 }
